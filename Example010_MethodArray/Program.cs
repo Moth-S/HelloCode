01266114cd578bde03961ffab14bb8ba94ea99c3
Lyubov Si <gse182@yandex.ru>
2022-09-01 14:56:30 +0300

    add 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 55, 48, 35, 61, 75, 48, 55};
+
+int n = array.Length;
+int find = 55;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
